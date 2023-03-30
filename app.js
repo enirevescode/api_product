cc9b1e37e5ad334397dca24acd606d2acf24ac70
enirevescode <enirevescode@gmail.com>
2023-03-30 11:49:20 +0200

    sup _helper ds app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan_logger = require('morgan')
-const helper = require('./_helper') //import juste la fonction success pas tt le fichier
+
 const sequelize = require('sequelize')
 const cors = require('cors')
 
