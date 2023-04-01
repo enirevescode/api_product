fca883794210de186be1797e94087490b1007a07
enirevescode <enirevescode@gmail.com>
2023-04-01 09:35:57 +0200

    supr helper

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const morgan_logger = require('morgan')
-const success = require('./_helper') //import juste la fonction success pas tt le fichier
+//const success = require('./_helper') //import juste la fonction success pas tt le fichier
 const sequelize = require('sequelize')
 const cors = require('cors')
 //const ProduitModel = require('./models/produit')
